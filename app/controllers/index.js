72b3150df552196e3051da513cf2ff81cce341fb
frankdressel <frankdressel@users.noreply.github.com>
2017-01-03 07:32:07 +0100

    First step towards eval alternative.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,12 +3,22 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
     actions: {
         generate() {
-            var code = Blockly.JavaScript.workspaceToCode(this.model.workspace)+`function create(){return new StatemachineObject();}`;
+            var code = '(function(){\n'+
+            '    '+Blockly.JavaScript.workspaceToCode(this.model.workspace)+'\n'+
+            '    var statemachine=new StatemachineObject();\n'+
+            '    return {\n'+
+            '        transit(){\n'+
+            '            statemachine.transit();\n'+    
+            '        },\n'+
+            '        set(key, value){eval(key+\'=\'+value);},\n'+
+            '        get(key){return eval(key);},\n'+
+            '        getAttributes(){return statemachine.attributes;}\n'+
+            '    }\n'+
+            '})()';
             console.log(code);
-            var geval = eval;
-            geval(code);
-            Ember.set(this.model, 'statemachine', create());
-            console.log(Object.getOwnPropertyNames(this.model.statemachine));
+            var statemachine=eval(code);
+            console.log(statemachine.getAttributes());
+            Ember.set(this.model, 'statemachine', statemachine);
         },
         transit() {
             let historical=Ember.get(this.model, 'historical');
