55999e8fabe7cb58f069d36f5dfb76ad105e4746
frankdressel <frankdressel@users.noreply.github.com>
2017-01-05 23:21:28 +0100

    Better layout

--- app/components/evaluation-container.js
+++ app/components/evaluation-container.js
@@ -2,9 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
     actions:{
-        transit(){
-            this.get('transit')();
-        },
         change(key){
             this.get('statemachine').set(key, arguments[1]); 
         }
